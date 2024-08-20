bc537bc2dba2c9a96c5b7b6ad28f0b9d6149391e
Tirth Ajagiya <tirthajagiya5618@gmail.com>
2024-08-20 23:21:44 +0530

    Time: 53 ms (39.4%), Space: 48.7 MB (45.31%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -1,5 +1,14 @@
-function createHelloWorld(){
-    return function(){
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
         return "Hello World";
-    };
-}
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
